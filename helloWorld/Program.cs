c231d4e4c69b11fabc6de0741f476f185007d7b7
Łukasz Dawidowicz <lddev@outlook.com>
2021-03-30 12:07:22 +0200

    Dodaj pliki projektów.

---
+++ helloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string name = "Mieszko";
+            float age = 1.5f;
+
+            int num1 = 20;
+            int num2 = 5;
+
+            int sum = num1 + num2;
+
+            Console.WriteLine("Hello " + name + ", masz " + age);
+
+            Console.WriteLine(name);
+
+            Console.WriteLine(age);
+
+            Console.WriteLine(sum);
+
+            Console.Read();
+        }
+    }
+}
