8adb09bbbd2207705d6c28c1dcecda2d0e5daf6a
Łukasz Dawidowicz <lddev@outlook.com>
2021-04-08 14:01:51 +0200

    data type conversion

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -31,6 +31,26 @@ namespace helloWorld
             Console.Read();
             */
 
+            //this will work. can assing smal values to biger vaules
+            int someNum = 459412;
+            long myLongNum = someNum;
+
+            float myFloat = 13.73F;
+            double myNewDouble = myFloat;
+
+
+            //not working shows only 13
+            double myDouble = 13.73;
+            int myInt;
+
+            //cast double to int - change init to mydouble only shows 13
+            myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+            Console.Read();
+
+
+            //convert type from num to string
+            string myString = myDouble.ToString();
         }
     }
 }
